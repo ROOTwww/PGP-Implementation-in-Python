0a7c78477b0f3f6fc482439fa9fd4acccb88afa8
ROOTwww <47178548+ROOTwww@users.noreply.github.com>
2022-07-16 20:06:17 +0300

    server

--- server.py
+++ server.py
@@ -7,40 +7,43 @@ Key_server_public, Key_server_private = RSA_Create_Keys()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen()
 
 print("Server started")
-conn, addr = s.accept()
+while True:
+    s.listen()
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
 
-conn.send(RSA_Serialize(Key_server_public))
+    conn.send(RSA_Serialize(Key_server_public))
 
-Key_client_public = RSA_deSerialize(conn.recv(315))
+    Key_client_public = RSA_deSerialize(conn.recv(315))
 
-ACK =RSA_Encrypt(Key_client_public, "ACK".encode("ascii"))
-conn.send(ACK)
+    ACK =RSA_Encrypt(Key_client_public, "ACK".encode("ascii"))
+    conn.send(ACK)
 
-ACK = conn.recv(315)
-if b"ACK" != RSA_Decrypt(Key_server_private, ACK):
-    print("ACK Error")
-    exit()
+    ACK = conn.recv(315)
+    if b"ACK" != RSA_Decrypt(Key_server_private, ACK):
+        print("ACK Error")
+        exit()
 
-data = conn.recv(992)
+    data = conn.recv(992)
 
-P1_Z2 = base64.b64decode(data)
+    P1_Z2 = base64.b64decode(data)
 
-crypt_P1_Z, crypt_aeskey = P1_Z2.split(b"::")
+    crypt_P1_Z, crypt_aeskey = P1_Z2.split(b"::")
 
-aeskey = RSA_Decrypt(Key_server_private, crypt_aeskey)
+    aeskey = RSA_Decrypt(Key_server_private, crypt_aeskey)
 
-P1_Z = AES_Decrypt(aeskey, crypt_P1_Z)
+    P1_Z = AES_Decrypt(aeskey, crypt_P1_Z)
 
-P1 = zlib.decompress(P1_Z)
+    P1 = zlib.decompress(P1_Z)
 
-crypt_hash_msg, msg = P1.split(b"::")
+    crypt_hash_msg, msg = P1.split(b"::")
 
-hash_msg = RSA_Decrypt(Key_server_private, crypt_hash_msg)
+    hash_msg = RSA_Decrypt(Key_server_private, crypt_hash_msg)
 
-if hash_msg != hashlib.md5(msg).hexdigest().encode("ascii"):
-    print("Hash error")
+    if hash_msg != hashlib.md5(msg).hexdigest().encode("ascii"):
+        print("Hash error")
 
-print("Message: ", msg.decode("ascii"))
+    print("Message: ", msg.decode("ascii"))
+    print("Connection closed to: ", addr)
